93b4ac44fcaa712e86b7d00a477d0035fa076198
PabloAbreu <108098406+pabloloko1277@users.noreply.github.com>
2022-09-16 16:33:05 -0300

    Add files via upload

--- 02-pythonaplic_letscode_csv.py
+++ 02-pythonaplic_letscode_csv.py
@@ -15,11 +15,31 @@ with open('brasil_covid.csv','r',encoding ='utf-8') as arquivo:
     for linha in linhas:
         linha = linha.split(',')
         print(linha)
-with open('users.csv','w', encoding = 'utf-8') as arquivo:
+with open('users.csv','w', encoding = 'utf-8', newline='') as arquivo:
     escritor = csv.writer(arquivo)
     escritor.writerow(['nome','sobrenome', 'email','genero'])
     escritor.writerow(['Pablo','Abreu', 'pato@gmai.com','masculino'])
 
 
+#exemplo de cadastro
 
+header = ['nome', 'sobrenome']
+dados = []
+opt = input('Escolha uma opção :\n1 - Cadastrar\n0 - Sair\n')
+while opt !='0':
+    nome = input('Qual seu nome?')
+    sobrenome = input('Qual seu sobre nome?')
+    dados.append([nome, sobrenome])
+    opt = input('Escolha uma opção :\n1 - Cadastrar\n0 - Sair\n')
+
+print(dados)
+
+with open('users.csv', 'w', newline='') as arquivo_csv:
+    writer = csv.writer(arquivo_csv)
+    writer. writerow(header)
+    writer.writerow(dados)
+with open('users.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    for row in csv_reader:
+        print(row)
 
